a6e7d45718d9f0e6ae9a6068ff259ede5da1403f
R-serban <nurdavletkr@gmail.com>
2022-11-14 11:11:01 +0500

    front 70%

--- front/src/App/App.js
+++ front/src/App/App.js
@@ -2,9 +2,24 @@
 import './App.css';
 import React from 'react';
 import {Navbar} from "../global/navbar";
+import Footer from '../Footer/Footer';
+import defaults from "../global/default.json";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {data: defaults};
+    this.st = this.st.bind(this);
+  }
+  st(data) {
+    this.setState({data: data})
+  }
+  async componentDidMount() {
+    let response = await fetch();
+    if(response.ok) {
+      this.st(await response.json());
+    } else {
+      console.log("fetch error");
+    }
   }
   render() {
     return (
@@ -13,10 +28,36 @@ class App extends React.Component {
         <header className="App-header">
           <Navbar />
         </header>
-        
-        <footer>
+        <div className='bodier'>
+          <div className='hats'>
+            
+             заметки
+          </div>
+          <div className='containern'>
+            <div className='notes'>
+              {this.state.data.map(data => {
+                return(
+                  <>
+                    <div className='note' style={{backgroundColor: data.color}}>
+                      {data.text}
+                    </div>
 
-        </footer>
+              </>    
+                );
+              })}
+ 
+            </div>   
+            <div className='tools'>
+              <form action='' method='POST' name='note'>
+                <textarea className='formtext' rows="10" cols="45"  name='text'  > Введите текст заметки</textarea>
+                <p> Выберите цвет <input type="color" name="color"/> </p>
+                <input type="submit" value="сохранить"/>
+              </form>
+            </div>      
+          </div>
+          
+        </div>
+        <Footer/>
       </div>
     );
   }
---
+++ front/src/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./footer.css";
+import g from "./g.png";
+import vk from "./vk.png";
+import inst from "./instagram.png";
+import fb from "./fb.png";
+class Footer extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return(
+            <footer class="footer">
+            <div class="container">
+            <div class="row">
+            <div class="footer-col">
+                <h4>Компания</h4>
+                <ul>
+                <li><a href="#">О нас</a></li>
+                <li><a href="#">наш сервис</a></li>
+                <li><a href="#">политика конфиденциальности</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Помощь</h4>
+                <ul>
+                <li><a href="#">ЧаВо</a></li>
+
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>Присоединяетесь</h4>
+                <div class="social-links">
+                <a href="#"><i class="fab fa-facebook-f"> <img src={g} className="icos" /> </i></a>
+                <a href="#"><i class="fab fa-twitter"> <img src={vk} className="icos" /></i></a>
+                <a href="#"><i class="fab fa-instagram"> <img src={inst} className="icos" />  </i></a>
+                <a href="#"><i class="fab fa-linkedin-in"> <img src={fb} className="icos" /> </i></a>
+                </div>
+            </div>
+            </div>
+            </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
